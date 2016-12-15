19399db0b58ff3b4f16fb9a53c8fdc49ac16ce80
Alex Cruz <mazdarider23@gmail.com>
2016-12-15 17:36:16 +0100

    Remove nfc_enhanced.mk
    Change-Id: I8b321adfd931f297355df756895e338721af8919

--- du.mk
+++ du.mk
@@ -23,7 +23,6 @@ $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := du_angler
 PRODUCT_DEVICE := angler
